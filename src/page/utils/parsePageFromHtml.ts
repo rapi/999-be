d9575cbd018636f020b7301132ef87de46a3d83e
Vasile Grinciuc <Vasile.Grinciuc@externals.adidas.com>
2021-08-06 14:17:06 +0300

    Parse pages

--- src/page/utils/parsePageFromHtml.ts
+++ src/page/utils/parsePageFromHtml.ts
@@ -1,11 +1,30 @@
 import { load } from 'cheerio';
+import { IPageItem } from '../IPageItem';
 
-export default (html: string): string[] => {
+export default (html: string): IPageItem[] => {
   const $ = load(html);
   const result = [];
 
-  $('.ads-list-photo-item-thumb a').each(function () {
-    result.push($(this).attr('href'));
+  $('.ads-list-detail-item').each(function () {
+    const price = $(this).find('.ads-list-detail-item-price').text();
+
+    result.push({
+      link: $(this).find('a').first().attr('href'),
+      title: $(this).find('.ads-list-detail-item-title ').first().text().trim(),
+      description: $(this).find('.ads-list-detail-item-intro').text().trim(),
+      price: parseInt(price.replace(/[^0-9]/g, '')),
+      noPrice: price.replace(/[0-9]/g, '').trim() === 'Договорная',
+      currency: price.replace(/[0-9]/g, '').trim(),
+      // .replace(/ /g, ''),
+      img: [
+        $(this)
+          .find('img')
+          .first()
+          ?.attr('data-src')
+          ?.replace('320x240', '900x900')
+          .split('?')[0],
+      ],
+    });
   });
-  return result.filter((link) => link.indexOf('booster') === -1);
+  return result.filter(({ link }) => link.indexOf('booster') === -1);
 };
