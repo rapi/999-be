cae9d96edd310bab8b86438cd0f711bc96ffc3ab
Vasile Grinciuc <vasilegrinciuc@ip-10-151-1-44.eu-central-1.compute.internal>
2022-10-19 20:21:01 +0300

    Clear tets

--- src/item/item.controller.spec.ts
+++ src/item/item.controller.spec.ts
@@ -2,21 +2,21 @@ import axios from 'axios';
 jest.setTimeout(1000000);
 describe('ItemController', () => {
   it('should be defined', async () => {
-    const ads = {};
-    for (let i = 1; i < 10; i++) {
-      const data = await fetch(
-        '/api/page?link=/ru/list/dating-and-greetings/i-need-a-man?page=' + i,
-      );
-      const elements = await data.json();
-      for (const j in elements) {
-        const dataPage = await fetch('/api/item?link=' + elements[j].link);
-        const item = await dataPage.json();
-        ads[item.link] = item;
-        console.log('[' + (parseInt(j) + (i - 1) * 83) + '/' + 83 * 9 + ']');
-      }
-    }
-    Object.values(ads)
-      .sort((a, b) => b.userMarkCount - a.userMarkCount)
-      .forEach((e) => console.log(e.link, e.userMark));
+    // const ads = {};
+    // for (let i = 1; i < 10; i++) {
+    //   const data = await fetch(
+    //     '/api/page?link=/ru/list/dating-and-greetings/i-need-a-man?page=' + i,
+    //   );
+    //   const elements = await data.json();
+    //   for (const j in elements) {
+    //     const dataPage = await fetch('/api/item?link=' + elements[j].link);
+    //     const item = await dataPage.json();
+    //     ads[item.link] = item;
+    //     console.log('[' + (parseInt(j) + (i - 1) * 83) + '/' + 83 * 9 + ']');
+    //   }
+    // }
+    // Object.values(ads)
+    //   .sort((a, b) => b.userMarkCount - a.userMarkCount)
+    //   .forEach((e) => console.log(e.link, e.userMark));
   });
 });
